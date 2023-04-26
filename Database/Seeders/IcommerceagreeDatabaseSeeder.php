2c19d4f6ba81b8095bddfb7b4dc921c5f1499de5
Cristian Marin <marin101998@gmail.com>
2023-04-26 12:16:52 -0500

    +add | modules seeds by jobs

--- Database/Seeders/IcommerceagreeDatabaseSeeder.php
+++ Database/Seeders/IcommerceagreeDatabaseSeeder.php
@@ -5,79 +5,84 @@ namespace Modules\Icommerceagree\Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 use Modules\Icommerce\Entities\ShippingMethod;
+use Modules\Isite\Jobs\ProcessSeeds;
 
 class IcommerceagreeDatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run($methodsFromOther=null)
-    {
-        Model::unguard();
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run($methodsFromOther = null)
+  {
+    Model::unguard();
+    ProcessSeeds::dispatch([
+      "baseClass" => "\Modules\Icommerceagree\Database\Seeders",
+      "seeds" => ["IcommerceagreeModuleTableSeeder"]
+    ]);
 
-        $this->call(IcommerceagreeModuleTableSeeder::class);
-        $methods = config('asgard.icommerceagree.config.methods');
+    $methods = config('asgard.icommerceagree.config.methods');
 
-        //Only the methods that match with the config
-        if(!is_null($methodsFromOther))
-            $methods = array_intersect_key($methods,$methodsFromOther);
+    //Only the methods that match with the config
+    if (!is_null($methodsFromOther))
+      $methods = array_intersect_key($methods, $methodsFromOther);
 
-        if(count($methods)>0){
+    if (count($methods) > 0) {
 
-            $init = "Modules\Icommerceagree\Http\Controllers\Api\IcommerceAgreeApiController";
+      $init = "Modules\Icommerceagree\Http\Controllers\Api\IcommerceAgreeApiController";
 
-            foreach ($methods as $key => $method) {
+      foreach ($methods as $key => $method) {
 
-                $result = ShippingMethod::where('name',$method['name'])->first();
+        $result = ShippingMethod::where('name', $method['name'])->first();
 
-                if(!$result){
+        if (!$result) {
 
-                    $options['init'] = $init;
+          $options['init'] = $init;
 
-                    $titleTrans = $method['title'];
-                    $descriptionTrans = $method['description'];
+          $titleTrans = $method['title'];
+          $descriptionTrans = $method['description'];
 
-                    $params = array(
-                        'name' => $method['name'],
-                        'status' => $method['status'],
-                        'options' => $options
-                    );
+          $params = array(
+            'name' => $method['name'],
+            'status' => $method['status'],
+            'options' => $options
+          );
 
-                    if(isset($method['parent_name']))
-                        $params['parent_name'] = $method['parent_name'];
+          if (isset($method['parent_name']))
+            $params['parent_name'] = $method['parent_name'];
 
-                    $shippingMethod = ShippingMethod::create($params);
+          $shippingMethod = ShippingMethod::create($params);
 
-                    $this->addTranslation($shippingMethod,'en',$titleTrans,$descriptionTrans);
-                    $this->addTranslation($shippingMethod,'es',$titleTrans,$descriptionTrans);
+          $this->addTranslation($shippingMethod, 'en', $titleTrans, $descriptionTrans);
+          $this->addTranslation($shippingMethod, 'es', $titleTrans, $descriptionTrans);
 
-                }else{
-                     $this->command->alert("This method: {$method['name']} has already been installed !!");
-                }
-            }
-
-        }else{
-           $this->command->alert("No methods in the Config File !!"); 
+        } else {
+          $this->command->alert("This method: {$method['name']} has already been installed !!");
         }
- 
-        
+      }
+
+    } else {
+      $this->command->alert("No methods in the Config File !!");
     }
 
-    /*
-    * Add Translations
-    * PD: New Alternative method due to problems with astronomic translatable
-    **/
-    public function addTranslation($shippingMethod,$locale,$title,$description){
 
-      \DB::table('icommerce__shipping_method_translations')->insert([
-          'title' => trans($title,[],$locale),
-          'description' => trans($description,[],$locale),
-          'shipping_method_id' => $shippingMethod->id,
-          'locale' => $locale
-      ]);
+  }
 
-    }
+  /*
+  * Add Translations
+  * PD: New Alternative method due to problems with astronomic translatable
+  **/
+  public function addTranslation($shippingMethod, $locale, $title, $description)
+  {
+
+    \DB::table('icommerce__shipping_method_translations')->insert([
+      'title' => trans($title, [], $locale),
+      'description' => trans($description, [], $locale),
+      'shipping_method_id' => $shippingMethod->id,
+      'locale' => $locale
+    ]);
+
+  }
 
 }
